9189ead05c10c9ad81f32f89c907749024380830
Tolga Ateşci <x-man_3737@hotmail.com>
2021-02-05 17:16:58 +0300

    05.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
@@ -10,42 +11,48 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal()); 
-            foreach (var car in carManager.GetAll())
+            //CarManager carManager = new CarManager(new InMemoryCarDal()); 
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //Console.WriteLine("-----------------------------------------");
+
+            //ICarDal carDal = new InMemoryCarDal();
+            //carDal.Add(new Car { Id = 4, BrandId = 444, ColorId = 44, ModelYear = 2013, DailyPrice = 250, Description = "rty" });
+
+            //foreach (var car in carDal.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //Console.WriteLine("-----------------------------------------");
+
+            //carDal.Update(new Car { Id = 3, BrandId = 555, ColorId = 55, ModelYear = 2018, DailyPrice = 520, Description = "fgh" });
+
+            //carDal.Delete(new Car { Id = 2 });
+
+
+            //foreach (var car in carDal.GetAll())
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //Console.WriteLine("-----------------------------------------");
+
+            //foreach (var car in carDal.GetAllById(1))
+            //{
+            //    Console.WriteLine(car.Description);
+            //}
+
+            //Console.WriteLine("-----------------------------------------");
+
+            CarManager carManager = new CarManager(new EfCarDal()); 
+            foreach (var car in carManager.GetCarsByColorId(11))
             {
-                Console.WriteLine(car.Description);
+                Console.WriteLine(car.DailyPrice);
             }
-
-            Console.WriteLine("-----------------------------------------");
-                       
-            ICarDal carDal = new InMemoryCarDal();
-            carDal.Add(new Car { Id = 4, BrandId = 444, ColorId = 44, ModelYear = 2013, DailyPrice = 250, Description = "rty" });
-
-            foreach (var car in carDal.GetAll())
-            {
-                Console.WriteLine(car.Description);
-            }
-
-            Console.WriteLine("-----------------------------------------");
-
-            carDal.Update(new Car { Id = 3, BrandId = 555, ColorId = 55, ModelYear = 2018, DailyPrice = 520, Description = "fgh" });
-
-            carDal.Delete(new Car { Id = 2 });
-
-
-            foreach (var car in carDal.GetAll())
-            {
-                Console.WriteLine(car.Description);
-            }
-
-            Console.WriteLine("-----------------------------------------");
-
-            foreach (var car in carDal.GetAllById(1))
-            {
-                Console.WriteLine(car.Description);
-            }
-
-            Console.WriteLine("-----------------------------------------");
         }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/AvisContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AvisContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-V4PGNQI;Database=Avis;Trusted_Connection=true");
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
