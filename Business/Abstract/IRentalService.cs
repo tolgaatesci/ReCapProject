6246f779b8f53c514c0021ac5d901558eef7bd68
Tolga Ateşci <x-man_3737@hotmail.com>
2021-02-11 16:39:34 +0300

    11.02.2021_Odev_4

--- Business/Abstract/IRentalService.cs
+++ Business/Abstract/IRentalService.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Business.Abstract
 {
-    public interface IRentalService : IEntitiesRepositoryService<Car>
+    public interface IRentalService : IEntitiesRepositoryService<Rental>
     {
     }
 }
